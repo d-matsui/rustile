07c55beb2e7cc3d86edc2653230a3d8d98613777
Daiki Matsui <mti.daiki@gmail.com>
2024-09-19 23:39:21 +0900

    learn winit: create multiple windows

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use winit::application::ApplicationHandler;
-use winit::event::{DeviceEvent, DeviceId, Event, WindowEvent};
+use winit::event::{DeviceEvent, DeviceId, WindowEvent};
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowId};
 
 struct MyUserEvent;
 
 struct State {
-    window: Window,
+    windows: Vec<Window>,
     counter: i32,
 }
 
 impl ApplicationHandler<MyUserEvent> for State {
-    fn user_event(&mut self, event_loop: &ActiveEventLoop, user_event: MyUserEvent) {
-        // Handle user event.
-    }
-
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         // Your application got resumed.
     }
@@ -44,17 +40,28 @@ impl ApplicationHandler<MyUserEvent> for State {
     }
 
     fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
-        self.window.request_redraw();
+        for window in &self.windows {
+            window.request_redraw();
+        }
         self.counter += 1;
     }
 }
 
 fn main() {
     let event_loop = EventLoop::<MyUserEvent>::with_user_event().build().unwrap();
-    let window = event_loop
-        .create_window(Window::default_attributes())
-        .unwrap();
-    let mut state = State { window, counter: 0 };
+
+    let mut windows = Vec::new();
+    for i in 0..3 {
+        let window = event_loop
+            .create_window(Window::default_attributes())
+            .unwrap();
+        windows.push(window);
+    }
+
+    let mut state = State {
+        windows,
+        counter: 0,
+    };
 
     let _ = event_loop.run_app(&mut state);
 }
