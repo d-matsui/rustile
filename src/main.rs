0ba914c6131122a2f68ddf51755e3ba88c9866bf
Daiki Matsui <mti.daiki@gmail.com>
2024-09-20 00:04:55 +0900

    fix unused var warnings

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,14 @@ struct State {
 }
 
 impl ApplicationHandler<MyUserEvent> for State {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+    fn resumed(&mut self, _event_loop: &ActiveEventLoop) {
         // Your application got resumed.
     }
 
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
-        window_id: WindowId,
+        _window_id: WindowId,
         event: WindowEvent,
     ) {
         // Handle window event.
@@ -32,14 +32,14 @@ impl ApplicationHandler<MyUserEvent> for State {
 
     fn device_event(
         &mut self,
-        event_loop: &ActiveEventLoop,
-        device_id: DeviceId,
-        event: DeviceEvent,
+        _event_loop: &ActiveEventLoop,
+        _device_id: DeviceId,
+        _event: DeviceEvent,
     ) {
         // Handle device event.
     }
 
-    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+    fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
         for window in &self.windows {
             window.request_redraw();
         }
@@ -51,7 +51,7 @@ fn main() {
     let event_loop = EventLoop::<MyUserEvent>::with_user_event().build().unwrap();
 
     let mut windows = Vec::new();
-    for i in 0..3 {
+    for _ in 0..3 {
         let window = event_loop
             .create_window(Window::default_attributes())
             .unwrap();
